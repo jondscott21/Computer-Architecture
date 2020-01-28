2d4661abb95b26f0dc151b8c0401d3e9160280e8
Jonathan Scott <jonathan.scott@gmail.com>
2020-01-28 13:06:26 -0800

    mult finished

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -14,26 +14,38 @@ class CPU:
         # Process counter -- pc
         self.pc = 0
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
+        try:
+            address = 0
+            with open(filename) as f:
+                for line in f:
+                    # print(line)
+                    comment_split = line.split("#")
+                    num = comment_split[0].strip()
+                    if num == "":
+                        continue
+                    instruction = int(num,2)
+                    self.ram[address] = instruction
+                    address += 1
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {filename} not found")
 
-        address = 0
+        # # For now, we've just hardcoded a program:
+        # address = 0
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -42,6 +54,12 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
+        elif op == "MULT":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "DIV":
+            self.reg[reg_a] /= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -76,6 +94,7 @@ class CPU:
         running = True
         LDI = 0b10000010
         PRN = 0b01000111
+        MULT = 0b10100010
         HLT = 0b00000001
         while running:
             operand_a = self.ram_read(self.pc + 1)
@@ -87,8 +106,11 @@ class CPU:
             elif IR == PRN:
                 print(self.reg[operand_a])
                 self.pc += 2
+            elif IR == MULT:
+                self.alu("MULT", operand_a, operand_b)
+                self.pc += 3
             elif IR == HLT:
                 running = False
-            else:
-                print(f"Unknown Command {IR}")
+            # else:
+            #     print(f"Unknown Command {IR}")
 
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -3,10 +3,20 @@
 """Main."""
 
 import sys
+import os
 from cpu import *
+
+
+# if len(sys.argv) != 2:
+os.path.join('ls8/examples')
+# from examples import mult
+print(sys.argv)
+if len(sys.argv) != 2:
+    print("Usage: file.py filename", file=sys.stderr)
+    sys.exit(1)
 
 cpu = CPU()
 
-cpu.load()
+cpu.load(sys.argv[1])
 cpu.run()
 cpu.trace()
